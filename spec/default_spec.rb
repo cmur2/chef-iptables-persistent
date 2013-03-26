d3c36ae8e1dd9a94429e6c0c5bb15d926c1f0e4f
cn <chrnicolai@gmail.com>
2013-03-26 11:57:21 +0100

    More specs

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -7,5 +7,22 @@ describe 'iptables-persistent::default' do
     expect(chef_run).to install_package 'iptables'
     expect(chef_run).to install_package 'iptables-persistent'
   end
+  
+  it "creates an init script" do
+    expect(chef_run).to create_cookbook_file "/etc/init.d/iptables-persistent"
+  end
+  
+  it "creates ipv4 rules" do
+    expect(chef_run).to create_cookbook_file "/etc/iptables/rules"
+  end
+  
+  it "creates ipv6 rules" do
+    expect(chef_run).to create_cookbook_file "/etc/iptables/rules.v6"
+  end
+  
+  it "enables and starts iptables-persistent" do
+    expect(chef_run).to start_service "iptables-persistent"
+    expect(chef_run).to set_service_to_start_on_boot "iptables-persistent"
+  end
 end
 
